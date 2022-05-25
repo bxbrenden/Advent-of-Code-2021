197391575d5bdb178fc853e7edf0070083bdb0b4
Brenden Hyde <brendenahyde@gmail.com>
2022-05-24 20:11:27 -0700

    solution to day 7, part 1

--- Day07/part1.py
+++ Day07/part1.py
@@ -19,10 +19,34 @@ def get_initial_state(puzzle_input):
     return([int(x) for x in puzzle_input.split(',')])
 
 
+def calculate_cost(puzzle_input):
+    """Calculate the cost of moving."""
+    # use giant number so total cost is initially very high
+    #   this is a hack :-/
+    total_cost = 1_000_000_000_000_000
+    home_base_index = 0
+
+    for index in range(len(puzzle_input)):
+        current_cost = 0
+        home_base = puzzle_input[index]
+        for p in puzzle_input:
+            cost = abs(p - home_base)
+            current_cost += cost
+
+        if current_cost < total_cost:
+            total_cost = current_cost
+            home_base_index = index
+
+    return (home_base_index, total_cost)
+
+
 def main():
-    puzzle_input = get_puzzle_input(sample=True)
+    # puzzle_input = get_puzzle_input(sample=True)
+    puzzle_input = get_puzzle_input()
     initial_state = get_initial_state(puzzle_input)
     print(initial_state)
+    cheapest_place, cost = calculate_cost(initial_state)
+    print(f'Cheapest place to move: {cheapest_place}, cost: {cost}')
 
 
 if __name__ == '__main__':
