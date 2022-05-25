f628485580960907d54480f819374e3b4a87e753
Brenden Hyde <brendenahyde@gmail.com>
2022-05-24 17:07:37 -0700

    add utility function

--- Day06/part1.py
+++ Day06/part1.py
@@ -14,9 +14,29 @@ def get_puzzle_input(sample=False):
         raise SystemExit(f'Could not open {puzzle_input}. Permission denied.')
 
 
+def get_initial_state(puzzle_input):
+    """Split by commas and return as a list of ints."""
+    return puzzle_input.split(',')
+
+
+def print_status(state, num_days, first_run=False):
+    """Print the status of the puzzle input's current value."""
+    fish = ','.join(state)
+    if first_run:
+        template = f'Initial State: {fish}'
+    elif num_days == 1:
+        template = f'After {num_days} day: {fish}'
+    else:
+        template = f'After {num_days} days: {fish}'
+    print(template)
+
+
 def main():
     puzzle_input = get_puzzle_input(sample=True)
-    print(puzzle_input)
+    initial_state = get_initial_state(puzzle_input)
+    print_status(initial_state, 0, first_run=True)
+    print_status(initial_state, 1)
+    print_status(initial_state, 2)
 
 
 if __name__ == '__main__':
