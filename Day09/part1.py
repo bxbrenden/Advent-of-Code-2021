951f5ec400c8e89ff0efedea40a1d8d0fae13017
Brenden Hyde <brendenahyde@gmail.com>
2022-05-25 09:28:37 -0700

    start of day 9, part 1

---
+++ Day09/part1.py
@@ -0,0 +1,29 @@
+def get_puzzle_input(sample=False):
+    """Get the input for the puzzle and return as a string."""
+    if sample:
+        puzzle_input = 'sample_input.txt'
+    else:
+        puzzle_input = 'input.txt'
+    try:
+        with open(puzzle_input, 'r') as si:
+            sample_input = si.read().strip()
+            return sample_input
+    except FileNotFoundError:
+        raise SystemExit(f'Could not open {puzzle_input}. No such file.')
+    except PermissionError:
+        raise SystemExit(f'Could not open {puzzle_input}. Permission denied.')
+
+
+def get_initial_state(puzzle_input):
+    """Parse the data using the specified format."""
+    pass
+
+
+def main():
+    puzzle_input = get_puzzle_input(sample=True)
+    # puzzle_input = get_puzzle_input()
+    print(puzzle_input)
+
+
+if __name__ == '__main__':
+    main()
