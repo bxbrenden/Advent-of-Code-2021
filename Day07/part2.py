1df4ad519119799f33a99d3130fa7bc79485cec9
Brenden Hyde <brendenahyde@gmail.com>
2022-05-24 20:28:14 -0700

    solution to day 7, part 2

---
+++ Day07/part2.py
@@ -0,0 +1,58 @@
+def get_puzzle_input(sample=False):
+    """Get the input for the puzzle and return as a string."""
+    if sample:
+        puzzle_input = 'sample_input.txt'
+    else:
+        puzzle_input = 'input.txt'
+    try:
+        with open(puzzle_input, 'r') as si:
+            sample_input = si.read().strip()
+            return sample_input
+    except FileNotFoundError:
+        raise SystemExit(f'Could not open {puzzle_input}. No such file.')
+    except PermissionError:
+        raise SystemExit(f'Could not open {puzzle_input}. Permission denied.')
+
+
+def get_initial_state(puzzle_input):
+    """Split by commas and make everything an int."""
+    return([int(x) for x in puzzle_input.split(',')])
+
+
+def calculate_cost(puzzle_input):
+    """Calculate the cost of moving."""
+    # use giant number so total cost is initially very high
+    #   this is a hack :-/
+    total_cost = 1_000_000_000_000_000
+    home_base_index = 0
+
+    for index in range(len(puzzle_input)):
+        print('\n\n')
+        current_cost = 0
+        home_base = puzzle_input[index]
+        for p in puzzle_input:
+            magnitude = abs(p - index)
+            one_greater = magnitude + 1
+            cost = (one_greater * magnitude) / 2
+            print(f'- Move from {p} to {home_base}: {cost} fuel')
+            current_cost += cost
+
+        print(current_cost)
+        if current_cost < total_cost:
+            total_cost = current_cost
+            home_base_index = index
+
+    return (home_base_index, total_cost)
+
+
+def main():
+    # puzzle_input = get_puzzle_input(sample=True)
+    puzzle_input = get_puzzle_input()
+    initial_state = get_initial_state(puzzle_input)
+    print(initial_state)
+    cheapest_place, cost = calculate_cost(initial_state)
+    print(f'Cheapest place to move: {cheapest_place}, cost: {cost}')
+
+
+if __name__ == '__main__':
+    main()
