0eb800dffd9a7693917c67d0090819461c8f0591
Brenden Hyde <brendenahyde@gmail.com>
2022-05-25 08:48:23 -0700

    solution to day 8, part 1

--- Day08/part1.py
+++ Day08/part1.py
@@ -16,32 +16,58 @@ def get_puzzle_input(sample=False):
 
 def get_initial_state(puzzle_input):
     """Parse the data using the specified format."""
-    spl = puzzle_input.split('|')
-    ten_sequence = spl[0].strip().split()
-    four_sequence = spl[1].strip().split()
+    all_sequences = []
+    lines = puzzle_input.split('\n')
+    print(lines, '\n\n')
+    for line in lines:
+        spl = line.split(' | ')
+        four_sequence = spl[1].strip().split()
+        all_sequences.append(four_sequence)
 
-    return (ten_sequence, four_sequence)
+    return all_sequences
 
 
 def find_unique_segments(line):
+    found = 0
     for index, entry in enumerate(line):
         if len(entry) == 4:
             print(f'Number 4 is represented by sequence {entry} at index {index}')
+            found += 1
         elif len(entry) == 2:
             print(f'Number 1 is represented by sequence {entry} at index {index}')
+            found += 1
         elif len(entry) == 3:
             print(f'Number 7 is represented by sequence {entry} at index {index}')
+            found += 1
         elif len(entry) == 7:
             print(f'Number 8 is represented by sequence {entry} at index {index}')
+            found += 1
+
+    return found
+
+
+def parse_lines(all_sequences):
+    """Given a list of two-tuples, parse them for unique entries."""
+    total = 0
+    for index, seq in enumerate(all_sequences):
+        print(f'Starting at line {index + 1}\n')
+        # ten_sequence = seq[0]
+        # four_sequence = seq[1]
+        # find_unique_segments(ten_sequence)
+        found = find_unique_segments(seq)
+        if found:
+            total += found
+        print('\n\n')
+
+    return total
 
 
 def main():
-    puzzle_input = get_puzzle_input(sample=True)
-    # puzzle_input = get_puzzle_input()
-    ten_sequence, four_sequence = get_initial_state(puzzle_input)
-    print(ten_sequence, four_sequence)
-    find_unique_segments(ten_sequence)
-    find_unique_segments(four_sequence)
+    # puzzle_input = get_puzzle_input(sample=True)
+    puzzle_input = get_puzzle_input()
+    all_sequences = get_initial_state(puzzle_input)
+    num_found = parse_lines(all_sequences)
+    print(f'\n\nFound a total of {num_found} sequences')
 
 
 if __name__ == '__main__':
